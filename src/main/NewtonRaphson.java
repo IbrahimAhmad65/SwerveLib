a95baf8a033f45cd606550c1c56431804f16bca6
IbrahimAhmad65 <ibrahim.algalaly@gmail.com>
2022-06-01 06:33:16 -0600

    amooooooogus

--- src/main/NewtonRaphson.java
+++ src/main/NewtonRaphson.java
@@ -7,7 +7,7 @@ public class NewtonRaphson {
 //        this.fPrime = fPrime;
     }
 
-    public static double approximate(Function<Double, Double> f, Function<Double, Double> fPrime,double initial, int iterations) {
+    public static double approximate( Function<Double, Double> f, Function<Double, Double> fPrime,double initial, int iterations) {
         double current = initial;
         for (int i = 0; i < iterations; i++) {
             current = current - ((Double) f.compute(current)).doubleValue() / ((Double) fPrime.compute(current)).doubleValue();
