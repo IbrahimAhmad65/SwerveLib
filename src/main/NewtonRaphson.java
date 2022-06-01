61f5f5e27102bc405fd445717dbadb933a89560b
IbrahimAhmad65 <ibrahim.algalaly@gmail.com>
2022-06-01 06:33:40 -0600

    amogus

--- src/main/NewtonRaphson.java
+++ src/main/NewtonRaphson.java
@@ -7,7 +7,7 @@ public class NewtonRaphson {
 //        this.fPrime = fPrime;
     }
 
-    public static double approximate( Function<Double, Double> f, Function<Double, Double> fPrime,double initial, int iterations) {
+    public static double approximate(Function<Double, Double> f, Function<Double, Double> fPrime, double initial, int iterations) {
         double current = initial;
         for (int i = 0; i < iterations; i++) {
             current = current - ((Double) f.compute(current)).doubleValue() / ((Double) fPrime.compute(current)).doubleValue();
@@ -21,7 +21,7 @@ public class NewtonRaphson {
         for (int i = 0; i < iterations; i++) {
             oldCurrent = current;
             current = current - ((Double) f.compute(current)).doubleValue() / ((Double) fPrime.compute(current)).doubleValue();
-            if (threshold.compute(current - oldCurrent) && (i+1) < iterations) {
+            if (threshold.compute(current - oldCurrent) && (i + 1) < iterations) {
                 System.out.println("Premature Exiting on Newtons Method at iterations= " + (i + 1) + " Target Iterations= " + iterations);
                 break;
             }
@@ -30,7 +30,7 @@ public class NewtonRaphson {
     }
 
     // Move all vars to left hand side, ie f and f prime righthand side is a changing number, use for faster runtime bc no reconstructing functions
-    public static double solve(Function<Double,Double> f, Function<Double,Double> fPrime, double initial, double iterations, double rightHandSide){
+    public static double solve(Function<Double, Double> f, Function<Double, Double> fPrime, double initial, double iterations, double rightHandSide) {
         double current = initial;
         for (int i = 0; i < iterations; i++) {
             current = current - (((Double) f.compute(current)).doubleValue() - rightHandSide) / ((Double) fPrime.compute(current)).doubleValue();
@@ -38,13 +38,13 @@ public class NewtonRaphson {
         return current;
     }
 
-    public static double smartSolve(Function<Double,Double> f, Function<Double,Double> fPrime, double initial, double iterations, double rightHandSide, Function<Boolean, Double> threshold){
+    public static double smartSolve(Function<Double, Double> f, Function<Double, Double> fPrime, double initial, double iterations, double rightHandSide, Function<Boolean, Double> threshold) {
         double current = initial;
         double oldCurrent = 0;
         for (int i = 0; i < iterations; i++) {
             oldCurrent = current;
             current = current - (((Double) f.compute(current)).doubleValue() - rightHandSide) / ((Double) fPrime.compute(current)).doubleValue();
-            if (threshold.compute(current - oldCurrent) && (i+1) < iterations) {
+            if (threshold.compute(current - oldCurrent) && (i + 1) < iterations) {
                 System.out.println("Premature Exiting on Newtons Method at iterations= " + (i + 1) + " Target Iterations= " + iterations);
                 break;
             }
@@ -54,19 +54,21 @@ public class NewtonRaphson {
 
 
     public static void main(String[] args) {
-        Function<Double,Double> f2 = (x) -> {
+        Function<Double, Double> f2 = (x) -> {
             return (x * Math.pow(2.718, x) - 16);
         };
 
-        Function<Double,Double> f = (x) -> {
+        Function<Double, Double> f = (x) -> {
             return (x * Math.pow(2.718, x));
         };
-        Function<Double,Double> fPrime = (x) -> {
+        Function<Double, Double> fPrime = (x) -> {
             return (x * Math.pow(2.718, x) + Math.pow(2.718, x));
         };
 //        NewNewton n = new NewNewton();
-        System.out.println(NewtonRaphson.approximate(f2,fPrime,1,12));
-        System.out.println(NewtonRaphson.solve(f,fPrime,1,12,16));
-        System.out.println(NewtonRaphson.smartSolve(f,fPrime,1,12,16,(x)->{return(Math.abs(x) < .0001);}));
+        System.out.println(NewtonRaphson.approximate(f2, fPrime, 1, 12));
+        System.out.println(NewtonRaphson.solve(f, fPrime, 1, 12, 16));
+        System.out.println(NewtonRaphson.smartSolve(f, fPrime, 1, 12, 16, (x) -> {
+            return (Math.abs(x) < .0001);
+        }));
     }
 }
