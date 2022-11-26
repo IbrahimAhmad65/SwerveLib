2b02f7dc5684c15409b6eea698cd8efebdef689d
IbrahimAhmad65 <ibrahim.algalaly@gmail.com>
2022-11-25 19:00:04 -0700

    added a little documentation

--- src/main/cas/FunctionCAS.java
+++
@@ -1,7 +0,0 @@
-package main.cas;
-
-public interface FunctionCAS extends Blob {
-    Blob computeSingle(Blob arg);
-
-    Blob computeMulti(Blob arg, Blob[] otherArgs);
-}
--- src/main/cas/Operation.java
+++
@@ -1,7 +0,0 @@
-package main.cas;
-
-public interface Operation extends Blob {
-
-    Blob operate();
-
-}
