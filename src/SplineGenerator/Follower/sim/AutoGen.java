2a2c1d847ccb29aed59bd9409144589fda80f791
IbrahimAhmad65 <ibrahim.algalaly@gmail.com>
2023-04-07 18:21:00 -0600

    im so cool this literally took me 3 hours and could be so powerful

--- src/SplineGenerator/Follower/sim/AutoGen.java
+++ src/SplineGenerator/Follower/sim/AutoGen.java
@@ -4,9 +4,20 @@ import SplineGenerator.Follower.SingleAuto;
 import SplineGenerator.Splines.PolynomicSpline;
 import SplineGenerator.Util.DControlPoint;
 import main.Vector2D;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONWriter;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AutoGen {
     private static HashMap<String, Vector2D> posMap;
@@ -47,15 +58,23 @@ public class AutoGen {
         posMap.put("gmp2", new Vector2D(-1.199, -.706));
         posMap.put("gmp3", new Vector2D(-1.199, -1.909));
         posMap.put("gmp4", new Vector2D(-1.199, -3.101));
+        angleMap.put("gmp1", 10.0);
+        angleMap.put("gmp2", -3.0);
+        angleMap.put("gmp3", 72.0);
+        angleMap.put("gmp4", 1.0);
 
 
         posMap.put("stationIn", new Vector2D(-5, -1.07));
         posMap.put("stationOut", new Vector2D(-4, -1.07));
+        angleMap.put("stationIn", 0.0);
+        angleMap.put("stationOut", 0.0);
+
+
 
         waypointMapAboutZero.put("PIH", new AutoGenWaypoint[]{new AutoGenWaypoint("placeConeHighInit", 0, transitSpeed * 1.5)});
         waypointMapAboutZero.put("cone", new AutoGenWaypoint[]{new AutoGenWaypoint("pickCone", -.8, transitSpeed), new AutoGenWaypoint("none", -.1, .25), new AutoGenWaypoint("none", .1, .25), new AutoGenWaypoint("unpick", .3, transitSpeed)});
         waypointMapAboutZero.put("cube", new AutoGenWaypoint[]{new AutoGenWaypoint("pickCubeA", -.7, transitSpeed), new AutoGenWaypoint("none", -.1, .9), new AutoGenWaypoint("none", .1, .9), new AutoGenWaypoint("unpick", .3, transitSpeed)});
-        waypointMapAboutZero.put("PH", new AutoGenWaypoint[]{new AutoGenWaypoint("PlaceHighCont", -.7, transitSpeed), new AutoGenWaypoint("none", -.3, transitSpeed / 3.0), new AutoGenWaypoint("dropWithWait", transitSpeed * .8 / 3.0, transitSpeed)});
+        waypointMapAboutZero.put("PH", new AutoGenWaypoint[]{new AutoGenWaypoint("PlaceHighCont", -.7, transitSpeed), new AutoGenWaypoint("none", -.3, transitSpeed / 3.0), new AutoGenWaypoint("dropWithWait",0, transitSpeed * .8 / 3.0)});
         waypointMapAboutZero.put("lock", new AutoGenWaypoint[]{new AutoGenWaypoint("lockSwerve", -.1, .6), new AutoGenWaypoint("none", -.5, .6)});
 
 
@@ -66,7 +85,7 @@ public class AutoGen {
      * The following is cone cube cone 2 auto from Charged up 2023 washatator
      * NAME,s1Co_PH,gmp1_cube,s2Co_PH,gmp2_cone
      */
-    public SingleAuto createAuto(String autoData) {
+    public void createAuto(String autoData) {
         PolynomicSpline out = new PolynomicSpline(2, 1);
         String[] args = autoData.split(",");
         ArrayList<String> argsList = new ArrayList<>();
@@ -74,26 +93,93 @@ public class AutoGen {
             argsList.add(arg);
         }
         String name = argsList.remove(0);
-        int numWaypoints = argsList.size();
+        int numControlPoints = argsList.size();
+        ArrayList<Vector2D> controlPoints = new ArrayList<>();
+        ArrayList<AutoGenWaypoint> waypoints = new ArrayList<>();
+        ArrayList<AutoGenAngle> angles = new ArrayList<>();
 
         boolean firstItrOfString = true;
-        for (int i = 0; i < numWaypoints; i++) {
+        for (int i = 0; i < numControlPoints; i++) {
             String arg = argsList.remove(0);
             String[] argSplit = arg.split("_");
             String pos = argSplit[0];
             String action = argSplit[1];
             out.addControlPoint(new DControlPoint(posMap.get(pos).toDVector()));
-            if (firstItrOfString && action.equals(initActions)) {
+            controlPoints.add(posMap.get(pos));
+            angles.add(new AutoGenAngle(i, angleMap.get(pos)));
+            if (firstItrOfString && action.equals(action)) {
                 action = initActions;
                 firstItrOfString = false;
+            }
+            for (AutoGenWaypoint waypoint : waypointMapAboutZero.get(action)) {
+                waypoint.setDeltaT(waypoint.getDeltaT()+i);
+                waypoints.add(waypoint);
             }
 
 
         }
-        return new SingleAuto(name, out, null);
+
+        // FIXME Ordering can be fixed by swapping to a JSONArray, kinda big change so dont do it now
+        JSONObject json = new JSONObject();
+        json.put("Name", name);
+        json.put("thetaOffset", 90.0);
+        json.put("Closed", false);
+
+
+        JSONArray controlPointMap = new JSONArray();
+        for (Vector2D controlPoint : controlPoints) {
+            JSONObject controlPointData = new JSONObject();
+            controlPointData.put("X", controlPoint.getX());
+            controlPointData.put("Y", controlPoint.getY());
+            controlPointData.put("Vx", 0);
+            controlPointData.put("Vy", 0);
+            controlPointData.put("order", 1);
+
+            controlPointMap.put(controlPointData);
+        }
+
+
+        JSONArray waypointMap = new JSONArray();
+        for (AutoGenWaypoint waypoint : waypoints) {
+            JSONObject waypointData = new JSONObject();
+            waypointData.put("t", waypoint.getDeltaT());
+            waypointData.put("speed", waypoint.getSpeed());
+            waypointData.put("Command", waypoint.getName());
+            waypointMap.put(waypointData);
+        }
+        JSONArray angleMap = new JSONArray();
+        for (AutoGenAngle angle : angles) {
+            JSONObject angleData = new JSONObject();
+            angleData.put("t", angle.getDeltaT());
+            angleData.put("speed", angle.getSpeed());
+            angleMap.put(angleData);
+        }
+
+
+        json.put("ControlPoints", controlPointMap);
+        json.put("Waypoints", waypointMap);
+        json.put("RequiredPoints", angleMap);
+        try {
+            FileWriter writer = new FileWriter("/home/ibrahim/robotics/SwerveLib/src/SplineGenerator/Follower/sim/autogen.json");
+            writer.write(json.toString());
+            writer.flush();
+//            json.write(new FileWriter("/home/ibrahim/autgen.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
+//        return null;
+    }
+
+    public static void main(String[] args) {
+        AutoGen autoGen = new AutoGen();
+        autoGen.createAuto("NAME,s1Co_PH,gmp1_cube,s2Co_PH,gmp2_cone");
     }
 
 }
+
+
+
 
 class AutoGenWaypoint {
     private double deltaT;
@@ -106,6 +192,19 @@ class AutoGenWaypoint {
         this.speed = speed;
     }
 
+
+    public void setDeltaT(double deltaT) {
+        this.deltaT = deltaT;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getDeltaT() {
         return deltaT;
     }
@@ -116,5 +215,33 @@ class AutoGenWaypoint {
 
     public String getName() {
         return name;
+    }
+}
+
+class AutoGenAngle {
+    private double deltaT;
+    private double speed;
+
+    AutoGenAngle(double deltaT, double angle) {
+
+        this.deltaT = deltaT;
+        this.speed = speed;
+    }
+
+
+    public void setDeltaT(double deltaT) {
+        this.deltaT = deltaT;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDeltaT() {
+        return deltaT;
+    }
+
+    public double getSpeed() {
+        return speed;
     }
 }
